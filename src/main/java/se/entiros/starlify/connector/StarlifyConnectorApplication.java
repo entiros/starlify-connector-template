9b96374f23dae3ad39ca7b4acb080e144fc1561d
Simon Eriksson <simon.eriksson@entiros.se>
2024-01-18 14:26:41 +0100

    Add Spring boot project template

---
+++ src/main/java/se/entiros/starlify/connector/StarlifyConnectorApplication.java
@@ -0,0 +1,12 @@
+package se.entiros.starlify.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlifyConnectorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StarlifyConnectorApplication.class, args);
+  }
+}
