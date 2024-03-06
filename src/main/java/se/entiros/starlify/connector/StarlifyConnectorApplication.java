495220e274806d06c1a4f7bfba72e52f6f760918
Pia Løtvedt <pia.lotvedt@entiros.se>
2024-03-06 15:29:28 +0100

    Added annotation to main class, added default values to application.yaml

--- src/main/java/se/entiros/starlify/connector/StarlifyConnectorApplication.java
+++ src/main/java/se/entiros/starlify/connector/StarlifyConnectorApplication.java
@@ -1,9 +1,11 @@
 package se.entiros.starlify.connector;
 
+import com.starlify.connector.EnableStarlifyConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableStarlifyConnector
 public class StarlifyConnectorApplication {
 
   public static void main(String[] args) {
