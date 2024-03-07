d8177436943d7014fb054c0606faa9e7002929a6
Pia Løtvedt <pia.lotvedt@entiros.se>
2024-03-07 08:08:14 +0100

    Added implementation instructions in the StarlifyConnectorPluginImpl

--- src/main/java/com/example/StarlifyConnectorPluginImpl.java
+++ src/main/java/com/example/StarlifyConnectorPluginImpl.java
@@ -12,20 +12,39 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class StarlifyConnectorPluginImpl implements StarlifyConnectorPlugin {
   @Override
-  public List<StarlifyReference> getReferences() {
+  public List<StarlifySystem> getSystems() {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getReferences'");
+    // Fetch systems from the data source.
+    // Build StarlifySystem objects. Each StarlifySystem must have an externalId and a name.
+    // A StarlifySystem can also have a description.
+    // All the StarlifySystem objects must be returned in a list.
+    throw new UnsupportedOperationException("Unimplemented method 'getSystems'");
   }
-
   @Override
   public List<StarlifyService> getServices() {
     // TODO Auto-generated method stub
+    // Fetch services from the data source.
+    // Build StarlifyService objects. Each StarlifyService must have an externalId and a name.
+    // A StarlifyService must also have a providerExternalId mapping
+    // to the provider system. The provider system must have either been created in the getSystems
+    // method or be a system that already exists in Starlify.
+    // All the StarlifyService objects must be returned in a list.
     throw new UnsupportedOperationException("Unimplemented method 'getServices'");
   }
 
   @Override
-  public List<StarlifySystem> getSystems() {
+  public List<StarlifyReference> getReferences() {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getSystems'");
+    // Fetch references from the data source.
+    // Build StarlifyReference objects. Each StarlifyReference must have an externalId and a name.
+    // A StarlifyReference must also have a sourceExternalId mapping
+    // to the source system. The source system must have either been created in
+    // the getSystems method or be a system that already exists in Starlify.
+    // The StarlifyReference must also have a target Identifiable with an external id mapping to the
+    // target service. The target service must have either been created in the getServices method or
+    // be a service that already exists in Starlify.
+    // All the StarlifyReference objects must be returned in a list.
+    throw new UnsupportedOperationException("Unimplemented method 'getReferences'");
   }
+
 }
